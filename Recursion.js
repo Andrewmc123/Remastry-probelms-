cebcb8ec620e4710716b9de183894e9e742e1276
Andrewmc123 <49758479+Andrewmc123@users.noreply.github.com>
2024-08-16 00:58:19 -0400

    Add files via upload

---
+++ Recursion.js
@@ -0,0 +1,23 @@
+what is recursion - recursion is when a function is called within itself to solve a probelm. 
+
+A base case - simply helps stop the recusion from infinitly continuing. if you want it to continue you can use recursive Case  
+
+A recursive step is you making a call to the function with modified arguments 
+
+stack overflow is almost how it sounds, but it keeps tracks of functionn calls and variables,  when there is an infinite recursion  preventing unexpected crashes and errors. 
+
+event loop tends to asynchronous operations and codeing execution. 
+
+a call stack tracks function calls and local variables. 
+
+
+now the measseg queue is a list of messages to be processed. each message has an associated function that gets called to handle the message. 
+
+
+Asynchronous is where operations are run independtly while the program continues to run while waiting on tasks to be completed. 
+
+now Synchronous is where task of a progam are executed one at a time. 
+
+
+IIFE which stands for (Immediately Invoked Funtion Expression)
+it is desgined to have a funciton defined and exectuted immedietly making things more faster and effient. IIFE is useful when running initialization code by execute set-up task immeditley. you can also run functions immeditely without requiring external calls.   
